e5fd7f34bea6e26c7f9912f09544a1039202678c
Lakshay Kaushik <lakshaykaushik40004@gmail.com>
2026-03-30 22:38:49 +0530

    Add FeatureFlagMiddleware to attach feature flags

--- feature_flags/middleware.py
+++ feature_flags/middleware.py
@@ -1,1 +1,17 @@
+class FeatureFlagMiddleware:
+    """
+    Middleware to attach feature flags to request.
+    """
 
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Attach flags to request object
+        request.feature_flags = {}
+
+        from .registry import FEATURE_FLAGS
+        request.feature_flags.update(FEATURE_FLAGS)
+
+        response = self.get_response(request)
+        return response
