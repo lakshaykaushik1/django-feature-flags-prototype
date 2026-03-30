9dee83bd107c29eb118a75eaad2bd8b41f93ea1d
Lakshay Kaushik <lakshaykaushik40004@gmail.com>
2026-03-30 22:39:43 +0530

    Update test_flags.py

--- tests/test_flags.py
+++ tests/test_flags.py
@@ -1,1 +1,16 @@
+import unittest
+from feature_flags.utils import is_enabled
 
+class TestFeatureFlags(unittest.TestCase):
+
+    def test_flag_enabled(self):
+        self.assertTrue(is_enabled("new_dashboard"))
+
+    def test_flag_disabled(self):
+        self.assertFalse(is_enabled("beta_feature"))
+
+    def test_flag_default(self):
+        self.assertFalse(is_enabled("unknown_flag"))
+
+if __name__ == "__main__":
+    unittest.main()
